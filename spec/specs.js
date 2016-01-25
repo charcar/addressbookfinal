71adb6e405b6e7099b9716ed4a58c2093df79529
Erik Tolentino and Michael Lor <michael.vang.1985@gmail.com>
2016-01-25 08:46:00 -0800

    Add fullName functionality and constructor and prototype

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,14 @@
-describe('userInput', function() {
-  it("is false for an input that is not a number.", function(){
-  expect(userInput("a")).to.equal(false);
+describe('Contact', function() {
+  it("creates a new contact with the given properties.", function(){
+  var testContact = new Contact ("Mike","Lor");
+  expect(testContact.firstName).to.equal("Mike");
+  expect(testContact.lastName).to.equal("Lor");
+  expect(testContact.addresses).to.eql([]);
   });
+
+  it("adds the fullName method to a contact", function(){
+  var testContact = new Contact ("Erik","Tolentino");
+  expect(testContact.fullName()).to.equal("Erik Tolentino");
+  });
+
 });

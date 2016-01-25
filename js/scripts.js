2ef4f4ed5a63cec6ada191cef5a1fd530784849e
Erik Tolentino and Michael Lor <tolentino3575@gmail.com>
2016-01-25 09:20:25 -0800

    add javascript and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
-function Contact(firstName, lastName) {
+function Contact(firstName, lastName, address) {
   this.firstName = firstName;
   this.lastName = lastName;
-  this.addresses = ['235 SE.Street Portland, OR'];
+  this.address = ['235 SE.Street Portland, OR'];
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedAddress = $("input#new-address").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedAddress);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+  });
+});
+
+
+});
